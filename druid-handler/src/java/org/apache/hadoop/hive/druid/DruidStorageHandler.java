1d76adeedd60a8f015b8c6cbca37226949fc11d4
Slim Bouguerra <slim.bouguerra@gmail.com>
2016-11-03 17:29:08 -0700

    adding the support for db insert

--- druid-handler/src/java/org/apache/hadoop/hive/druid/DruidStorageHandler.java
+++ druid-handler/src/java/org/apache/hadoop/hive/druid/DruidStorageHandler.java
@@ -17,9 +17,15 @@
  */
 package org.apache.hadoop.hive.druid;
 
-import java.io.IOException;
-import java.util.List;
-
+import com.google.common.base.Suppliers;
+import com.google.common.base.Throwables;
+import io.druid.indexer.SQLMetadataStorageUpdaterJobHandler;
+import io.druid.metadata.MetadataStorageConnectorConfig;
+import io.druid.metadata.MetadataStorageTablesConfig;
+import io.druid.metadata.SQLMetadataConnector;
+import io.druid.metadata.storage.mysql.MySQLConnector;
+import io.druid.segment.loading.SegmentLoadingException;
+import io.druid.timeline.DataSegment;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -33,47 +39,87 @@ import org.apache.hadoop.hive.ql.io.DruidOutputFormat;
 import org.apache.hadoop.hive.ql.io.DruidOutputFormatUtils;
 import org.apache.hadoop.hive.ql.io.DruidQueryBasedInputFormat;
 import org.apache.hadoop.hive.ql.metadata.DefaultStorageHandler;
+import org.apache.hadoop.hive.ql.session.SessionState;
 import org.apache.hadoop.hive.serde2.SerDe;
 import org.apache.hadoop.mapred.InputFormat;
 import org.apache.hadoop.mapred.OutputFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Throwables;
-
-import io.druid.segment.loading.SegmentLoadingException;
-import io.druid.timeline.DataSegment;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * DruidStorageHandler provides a HiveStorageHandler implementation for Druid.
  */
-@SuppressWarnings({"deprecation","rawtypes"})
-public class DruidStorageHandler extends DefaultStorageHandler implements HiveMetaHook {
+@SuppressWarnings({"deprecation", "rawtypes"})
+public class DruidStorageHandler extends DefaultStorageHandler implements HiveMetaHook
+{
 
   protected static final Logger LOG = LoggerFactory.getLogger(DruidStorageHandler.class);
 
+  private final SQLMetadataConnector connector;
+  private final SQLMetadataStorageUpdaterJobHandler druidSqlMetadataStorageUpdaterJobHandler;
+  private final MetadataStorageTablesConfig druidMetadataStorageTablesConfig = MetadataStorageTablesConfig.fromBase("");
+
+  public DruidStorageHandler()
+  {
+    final String dbType = SessionState.getSessionConf().get("hive.druid.metadata.db.type", "mysql");
+    final String username = SessionState.getSessionConf().get("hive.druid.metadata.username", "");
+    final String password = SessionState.getSessionConf().get("hive.druid.metadata.password", "");
+    final String uri = SessionState.getSessionConf().get("hive.druid.metadata.uri", "jdbc:mysql://localhost/druid");
+
+    connector = new MySQLConnector(Suppliers.<MetadataStorageConnectorConfig>ofInstance(new MetadataStorageConnectorConfig()
+    {
+      @Override
+      public String getConnectURI()
+      {
+        return uri;
+      }
+
+      @Override
+      public String getUser()
+      {
+        return username;
+      }
+
+      @Override
+      public String getPassword()
+      {
+        return password;
+      }
+    }), Suppliers.ofInstance(druidMetadataStorageTablesConfig));
+
+    druidSqlMetadataStorageUpdaterJobHandler = new SQLMetadataStorageUpdaterJobHandler(connector);
+  }
+
   @Override
-  public Class<? extends InputFormat> getInputFormatClass() {
+  public Class<? extends InputFormat> getInputFormatClass()
+  {
     return DruidQueryBasedInputFormat.class;
   }
 
   @Override
-  public Class<? extends OutputFormat> getOutputFormatClass() {
+  public Class<? extends OutputFormat> getOutputFormatClass()
+  {
     return DruidOutputFormat.class;
   }
 
   @Override
-  public Class<? extends SerDe> getSerDeClass() {
+  public Class<? extends SerDe> getSerDeClass()
+  {
     return DruidSerDe.class;
   }
 
   @Override
-  public HiveMetaHook getMetaHook() {
+  public HiveMetaHook getMetaHook()
+  {
     return this;
   }
 
   @Override
-  public void preCreateTable(Table table) throws MetaException {
+  public void preCreateTable(Table table) throws MetaException
+  {
     // Do safety checks
     if (MetaStoreUtils.isExternalTable(table) && !StringUtils.isEmpty(table.getSd().getLocation())) {
       throw new MetaException("LOCATION may not be specified for Druid existing sources");
@@ -87,7 +133,8 @@ public class DruidStorageHandler extends DefaultStorageHandler implements HiveMe
   }
 
   @Override
-  public void rollbackCreateTable(Table table) throws MetaException {
+  public void rollbackCreateTable(Table table) throws MetaException
+  {
     // Nothing to do
     final Path segmentDescriptorDir = new Path(table.getSd().getLocation());
     try {
@@ -110,7 +157,8 @@ public class DruidStorageHandler extends DefaultStorageHandler implements HiveMe
   }
 
   @Override
-  public void commitCreateTable(Table table) throws MetaException {
+  public void commitCreateTable(Table table) throws MetaException
+  {
     //@TODO // FIXME: 10/31/16 get the actual outPutPath
     final Path segmentDescriptorDir = new Path(table.getSd().getLocation());
     try {
@@ -135,7 +183,7 @@ public class DruidStorageHandler extends DefaultStorageHandler implements HiveMe
 
         if (!fs.exists(path)) {
           LOG.warn("Segment Path [%s] does not exist. It appears to have been deleted already.", path);
-          return ;
+          return;
         }
 
         // path format -- > .../dataSource/interval/version/partitionNum/xxx.zip
@@ -182,27 +230,36 @@ public class DruidStorageHandler extends DefaultStorageHandler implements HiveMe
 
   private void publishSegments(List<DataSegment> publishedSegments)
   {
-    //insert into db
+
+    druidSqlMetadataStorageUpdaterJobHandler.publishSegments(
+        druidMetadataStorageTablesConfig.getSegmentsTable(),
+        publishedSegments,
+        DruidStorageHandlerUtils.JSON_MAPPER
+    );
     return;
   }
 
   @Override
-  public void preDropTable(Table table) throws MetaException {
+  public void preDropTable(Table table) throws MetaException
+  {
     // Nothing to do
   }
 
   @Override
-  public void rollbackDropTable(Table table) throws MetaException {
+  public void rollbackDropTable(Table table) throws MetaException
+  {
     // Nothing to do
   }
 
   @Override
-  public void commitDropTable(Table table, boolean deleteData) throws MetaException {
+  public void commitDropTable(Table table, boolean deleteData) throws MetaException
+  {
     // Nothing to do
   }
 
   @Override
-  public String toString() {
+  public String toString()
+  {
     return Constants.DRUID_HIVE_STORAGE_HANDLER_ID;
   }
 
