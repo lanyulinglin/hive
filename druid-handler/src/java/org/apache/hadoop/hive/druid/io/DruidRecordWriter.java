fd935068d7310f3ec172a833fbae391c330cde60
Slim Bouguerra <slim.bouguerra@gmail.com>
2016-11-10 17:08:11 -0800

    debug

--- druid-handler/src/java/org/apache/hadoop/hive/druid/io/DruidRecordWriter.java
+++ druid-handler/src/java/org/apache/hadoop/hive/druid/io/DruidRecordWriter.java
@@ -228,6 +228,8 @@ public class DruidRecordWriter implements RecordWriter<NullWritable, DruidWritab
       }
     });
 
+    LOG.info(String.format("event is [%s]", event));
+
     InputRow inputRow = new MapBasedInputRow(
             timestamp,
             dataSchema.getParser()
